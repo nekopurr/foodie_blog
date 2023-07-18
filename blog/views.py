2af0db59d4b35f04b4fd5c7b555ec457748f27e9
nekopurr <rksk0071@naver.com>
2023-07-18 17:28:21 +0900

    PostListView, PostDetailView, CommentCreateView 작성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Post, Comment
+from .forms import CommentForm
 
 # Create your views here.
+
+### Post
+class PostListView(View):
+
+    def get(self, request):
+        posts = Post.objects.all()
+        context = {
+            "post": posts,
+            # "title": "Blog",
+        }
+        return render(request, 'blog/post_list.html', context)
+
+
+class PostDetailView(View):
+    def get(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            context = {
+            'post': post,
+            'comments': comments,
+            'comment_form': comment_form
+            }
+            comments = Comment.objects.filter(post=post)
+            comment_form = CommentForm()
+            return render(request, 'blog/post_detail.html', context)
+        except Post.DoesNotExist:
+            return redirect('blog:post_list')
+
+
+class CommentCreateView(View):
+    def post(self, request, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = post
+                comment.save()
+            return redirect('blog:post_detail', pk=post.pk)
+        except Post.DoesNotExist:
+            return redirect('blog:post_list')
